7ef3679df10776e71c8f4b647f5ff9f6c06a5b52
Andrea Edwards <aedwards@xula.edu>
2020-10-06 21:03:09 -0500

    Add a bunch of TODOs

--- main.java
+++ main.java
@@ -5,19 +5,34 @@ import java.lang.reflect.Array;
 import java.util.*;
 
 public class main {
+  // TODO csvHeaders is only used in saveSimulatedInfoCSVfile so it shouldn't be a class variable.
   static String csvHeaders = "id, name, gpa";
   public static void main(String[] args) {
+
+    // TODO This function doesn't pass the "smell test".  Something seems wrong about it.
+    // It's called "createSimulatedDate" but doesn't require anything from the caller.
+    // How does the function know how many records of data the user wants?
+    // What's really odd is you have a "simulatedData" class but this is a function not a method.
+    // Shouldn't a simulatedData object know how to create simulated date? 
     ArrayList<simulatedData>  simulatedStudentData = createSimulatedData();
     System.out.println(simulatedStudentData.size());
+
+    // TODO This is odd.  Why do users create an ArrayList of simulatedData
+    // then write that data to a file.  Isn't it the other way around?
+    // The file has a bunch of data that is sometimes read into an ArrayList.
     saveSimulatedInfoCSVfile("studentData.csv", simulatedStudentData);
 
     
   }
 
+  // TODO Odd, you have a class that does stuff with simulate data but this is a function not a method.
   static void saveSimulatedInfoCSVfile(String csvFileName, ArrayList<simulatedData> simulatedStudentData) {
+
+    // TODO Use try-with-resources.
+    // https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
     try {
         PrintWriter pw = new PrintWriter(csvFileName);
-        System.out.println("Saving records");
+        System.out.println("Saving records");  // TODO Don't write your output/debugging messages in code that others use.
         pw.println(csvHeaders); // must write the headers before anything else
         for (simulatedData SD : simulatedStudentData) {
             pw.println(SD.toCSVstring());
@@ -26,12 +41,15 @@ public class main {
     } catch (FileNotFoundException e) {
         e.printStackTrace();
     }
-    System.out.println("Records Saved");
+    System.out.println("Records Saved");  // TODO Remove your output/debugging messages.
 }
 
+  // TODO Odd, you have a class that does stuff with simulate data but this is a function not a method.
   private static ArrayList<simulatedData> createSimulatedData(){
     ArrayList<simulatedData> dataArray = new ArrayList<>();
-    while(dataArray.size()<3325){
+
+    // TODO Don't use magic numbers.
+    while(dataArray.size()<3325){  // Java let's you write number literals with _ for readability.  3_325
       dataArray.add(new simulatedData());
     }
     return dataArray;
--- simulatedData.java
+++ simulatedData.java
@@ -1,29 +1,55 @@
 import java.util.*;
-class simulatedData {
-  private int ID;
-  private Double gpa;
-  private String name;
-  private Random rand = new Random();
 
+// TODO The convention is that class names are in TitleCase not camelCase.
+
+// TODO What an odd name for a class.  If a class is named String, I assume it holds characters and does String-like stuff.
+// I can't imagine what a "simulatedData" object holds or what operations are simulatedData-like.
+// Every class must have a single purpose.  The String class does String stuff.  What's the purpose for simulatedData?
+class simulatedData {
+  private int ID; // TODO The convention is that variables are camelCase, ClassNames in TitleCase, and CONSTANTS all caps.
+  private Double gpa; // TODO Don't use Double use double.  Google "Java box and unbox".
+  private String name;
+  private Random rand = new Random();  // TODO why is this a class variable?
+
+  // TODO You'll get more interesting names if you randomly build the name from a list of first names + list of last names.
+  // Also, this better models the human world and it's helpful when code models our human expectations.
+  // Why is this a class variable?
   private String[] NAMES = { 
     "Oliva Barch", "Amy Wong", "Timothy Brown", 
     "Jo Joestar", "Damion Mcarthy", "Greg foster",
     "Tina Turner", "Amy Winehouse", "Oliver Green",
     "Danny Zuko" };
 
+  // TODO This should have an @Override annotation to help the compiler.
+  // TODO Nope, don't decide for your users the labels of the output.
+  // "name \t id \t gpa"
   public String toString(){
     return name+", Student: "+ID+" G.P.A: "+gpa;
   }
 
+  // TODO You're using the word "simulated" when I think you mean "generate".
+  // This method generates a row of data, a record.
   public simulatedData(){
     this.name = NAMES[rand.nextInt(NAMES.length)];
+
+    // TODO Don't use "magic numbers".  Magic numbers are literal numbers (like 5, 10, 9000) that just magically appear.
+    // Use CONSTANTS.
     Double gpa = Double.parseDouble(rand.nextInt(5)+"."+rand.nextInt(10));
+    
+    // TODO  Yuck.  You're generating this gpa.  If you don't want a gpa > 4 then just don't generate a gpa > 4.
     while(gpa>4.0){
       gpa = Double.parseDouble(rand.nextInt(5)+"."+rand.nextInt(10));
     }
-    this.gpa = gpa;
+    this.gpa = gpa;  // TODO Odd, why do you need a local variable?
+
+    // TODO  Yuck.  Java knows how to generate random integers, why generate individual digits?
+    // Don't let your code wrap.  Keep lines around 80 characters.
     this.ID = Integer.parseInt("900" + rand.nextInt(10)+rand.nextInt(10)+rand.nextInt(10)+rand.nextInt(10)+rand.nextInt(10)+rand.nextInt(10));
   }
+
+  // TODO Don't write methods until you need them.
+  // More importantly, don't write methods that you haven't tested for correctness.
+  // YAGNI == "You ain't gonna need it"
 
   public int getID(){
     return this.ID;
