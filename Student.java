e52e4881ca7058247cb312eab9e7e26570a829fb
zyelcrier <65629240+zyelcrier@users.noreply.github.com>
2020-12-21 03:25:24 -0800

    Beginning TODOS round 3

--- Student.java
+++ Student.java
@@ -1,50 +1,35 @@
 import java.util.*;
 
 class Student {
-  private int iD;  // TODO  Fix the case since the convention for variables is camelCase.
+  private int iD; // TODO Fix the case since the convention for variables is camelCase.
   private float gpa;
   private String firstName;
   private String lastName;
 
-  @Override
-  public String toString() {
-    return "firstName\tlastName\t id \t gpa";
-  }
+  int GPA_MAX = 4;
+  int ID_MIN = 900_000;
+  int ID_MAX = 999_999;
+  String[] FIRST = { "Jamie", "Phil", "Cathy", "Jack", "Jen", "Tanner", "David", "Carter", "Molly", "Emily", "Roger",
+      "Tony", "Abby", "Davin","Chris","Abby","Thomas" };
+  String[] LAST = { "Barch", "Wong", "Brown", "Joestar", "Mcarthy", "clint", "foster", "Zuko", "Winehouse", "Green",
+      "Rose", "Walter" , "Rose-Wall"};
 
-  // TODO  The convention is to position constructors before every other method.
   public Student() {
     Random rand = new Random();
-    float gpaMax = (float) 4.0;
 
-    // TODO These are class constants, move them to Line 8.
-    // The convention is that CONSTANTS should be all caps.
-    // TODO  Java let's you put underscores in numbers to make them easier to read.
-    //    int ID_MIN = 900_000;  // BTW Java doesn't enforce the position of underscores, 9_00_0000
-    Integer idMin = 900000;
-    Integer idMax = 999999;
+    this.firstName = FIRST[rand.nextInt(FIRST.length)];
+    this.lastName = LAST[rand.nextInt(LAST.length)];
 
-    // TODO These are class constants.
-    String[] first = { "Jamie", "Phil", "Cathy", "Jack", "Jen", "Tanner", "David", "Carter", "Molly", "Emily", "Roger",
-        "Tony", "Abby" };
-    String[] last = { "Barch", "Wong", "Brown", "Joestar", "Mcarthy", "clint", "foster", "Zuko", "Winehouse", "Green",
-        "Rose", "Walter" };
-    
-    this.firstName = first[rand.nextInt(first.length)];
-    this.lastName = last[rand.nextInt(last.length)];
-
-    // TODO If this calculation, no one will have a 4.0 gpa.  Why?
-    // Google "javadocs 13 random nextfloat",
-    // click the Oracle link, then click nextfloat to see [0..GPA_MAX), inclusive-exclusive.
-
-    // TODO What can we do to generate a float in range?  Here's an algorithm:
+    // TODO What can we do to generate a float in range? Here's an algorithm:
     // use rand.nextFloat() then add a random int to it.
     // Java makes us work way too hard for this common task!
-    this.gpa = rand.nextFloat() * gpaMax;
+    this.gpa = rand.nextFloat() + rand.nextInt(GPA_MAX);
 
-    this.iD = rand.nextInt((idMax - idMin) + 1) + idMin;
+    this.iD = rand.nextInt((ID_MAX - ID_MIN) + 1) + ID_MIN;
   }
 
-  public String toCSVstring() {
+  @Override
+  public String toString() {
     return String.format("%d,%s,%s,%f,", iD, firstName, lastName, gpa);
   }
 
--- dataDriver.java
+++ dataDriver.java
@@ -1,45 +1,22 @@
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.*;
 
-// TODO The conventions are to use TitleCase for class names,
-// use camelCase for variables, and
-// use ALL_CAPS for constants.
-public class dataDriver {
+public class DataDriver {
 
   public static void main(String[] args) {
-    Scanner kb = new Scanner(System.in);
-
-    // TODO I shouldn't have to type a value when the problem statement says 3325 students.
-    System.out.println("How much Student data would you like to generate?");
-
-    // TODO  Scanner is smart enough to read an int without you having to read it as a String.
-    // TODO Don't use Integer when int will do.  Google "Java box an integer" and read about autoboxing.
-    // Try  int numOfReqData = kb.nextInt();
-    Integer numOfReqData = Integer.parseInt(kb.nextLine());
-
-    saveSimulatedDataCSVfile("studentData.csv", numOfReqData);
-    kb.close();
+    int NUM_OF_DATA = 3325;
+    saveSimulatedDataCSVfile("studentData.csv", NUM_OF_DATA);
 
   }
 
   // TODO  Use whitespace (empty lines, tabs, spacing) to group logical blocks of code.
-  // TODO  Don't use the class Integer when an int will do.
-  static void saveSimulatedDataCSVfile(String csvFileName, Integer numOfData) {
-    String csvHeaders = "id, First Name, Last Name, gpa";
+  static void saveSimulatedDataCSVfile(String csvFileName, int numOfData) {
 
     try (PrintWriter pw = new PrintWriter(csvFileName)){
 
-      // TODO  NO!!!!  Don't do things the problem statement doesn't require.
-      // By putting this header in our team's data file, you'll break other's code
-      pw.println(csvHeaders); // must write the headers before anything else
-
       for (int i = 0; i < numOfData; i++) {
         Student sd = new Student();
-
-        // TODO  Odd, why don't you just use
-        // pw.println(sd);
-        pw.println(sd.toCSVstring());
+        pw.println(sd);
       }
     } catch (FileNotFoundException e) {
       e.printStackTrace();
